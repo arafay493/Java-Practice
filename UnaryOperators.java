e2163e895ae1fcf56623ba97809e1e36a702e927
arafay493 <arafay493@gmail.com>
2024-09-11 07:44:19 +0500

    Learned Uranary Operators

---
+++ UnaryOperators.java
@@ -0,0 +1,20 @@
+public class UnaryOperators {
+    public static void main(String[] args) {
+        int x = 5;
+        // int y = -x;
+        // int y = x++;
+        // int y = ++x;
+        // System.out.println(y);
+        // int y = x--;
+        int y = --x;
+        System.out.println(y);
+
+        int a = 5;
+        System.out.println(++a);
+        a = a + 5;
+        System.out.println(a);
+        a += 10;
+        System.out.println(a);
+        
+    }
+}
