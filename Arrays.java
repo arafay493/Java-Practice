761e6d8be584d312bf06a5d1676a74f6b9ee09e9
arafay493 <arafay493@gmail.com>
2024-09-18 08:41:46 +0500

    Started Learning Arrays.

---
+++ Arrays.java
@@ -0,0 +1,15 @@
+public class Arrays {
+    public static void main(String[] args) {
+        int[] myInts = new int[5];
+        int[] myArr = {1, 2 ,3 ,4 ,5 ,6 ,7 ,8 , 9, 10};
+        System.out.println(myArr[0]);
+
+        // Declare and initialize an array
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        // Access and print elements of the array
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Element at index " + i + ": " + numbers[i]);
+        }
+    }
+}
