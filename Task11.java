ecd80e9dde4609a10ce4323129d3c14c32374f20
arafay493 <arafay493@gmail.com>
2024-10-05 10:11:26 +0500

    Done with some of the problems of task11.

--- Task11.java
+++ Task11.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class Task11 {
     public static void main(String[] args) {
         Task11 task = new Task11();
-        task.Checker();
+        // task.Checker();
+        task.Guess();
     }
 
     //! Create a program to using do While loop to find password checker until a valid password is entered.
@@ -70,5 +71,38 @@ public class Task11 {
 
     public void Checker(){
         PasswordChecker();
+    }
+
+    //! Create a program to using do While loop for number guessing game.
+    private static void GuessNumber() {
+        try (Scanner scanner = new Scanner(System.in)) {
+            int numbers[] = {45, 62 , 73, 90, 100, 44, 88};
+            boolean correct = false;
+            do {
+                System.out.println("Enter Number: ");
+                int number = scanner.nextInt();
+
+                int i = 0;
+                do {
+                    if (number == numbers[i]) {
+                        correct = true;
+                        break;
+                    }
+                    i++;
+                } while (i < numbers.length);
+                
+                if (correct) {
+                    System.out.println("Number Guess Correct!");
+                    break;
+                } else {
+                    System.out.println("Wrong Number.");
+                }
+            } while (true);
+        }
+    }
+    
+
+    public void Guess(){
+        GuessNumber();
     }
 }
