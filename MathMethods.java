da1f4fcecdf61c6a3aac11a9c18d4db99567a89c
arafay493 <arafay493@gmail.com>
2024-10-08 07:48:17 +0500

    Learned Common Math methods.

---
+++ MathMethods.java
@@ -0,0 +1,16 @@
+class MathMethods {
+    public static void main(String[] args) {
+        // System.out.println(Math.abs(-1));
+        // System.out.println(Math.ceil(9.01));
+        // System.out.println(Math.floor(9.01));
+        // System.out.println(Math.round(9.44));
+        // System.out.println(Math.round(9.45));
+        // System.out.println(Math.round(9.56));
+        // System.out.println(Math.random() * 3);
+        
+        // int random = (int) Math.round(Math.random() * 100);
+        // System.out.println(random);
+
+        System.out.println(Math.PI);
+    }
+}
