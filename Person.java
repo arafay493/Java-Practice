8a34de1666ec25eefb8901371f2ea5916dca18ae
arafay493 <arafay493@gmail.com>
2024-10-09 08:11:07 +0500

    Learned Final Keyword.

---
+++ Person.java
@@ -0,0 +1,7 @@
+public class Person {
+    public final String name = "John";
+    //constructor
+    public Person(String name) {
+        //this.name = name; //todo: cannot change the value of final
+    } 
+}
